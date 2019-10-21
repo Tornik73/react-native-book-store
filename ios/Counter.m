1772dab1f8adb642b1a0706a1af418f3d1e257c0
Andrii Dashkovets <andriidashkovets@Andriis-MacBook-Air3.local>
2019-10-21 18:44:26 +0300

    Added connection to native swift

---
+++ ios/Counter.m
@@ -0,0 +1,9 @@
+//
+//  Counter.m
+//  bsStore
+//
+//  Created by Andrii Dashkovets on 21.10.2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/Counter.swift
@@ -0,0 +1,9 @@
+//
+//  Counter.swift
+//  bsStore
+//
+//  Created by Andrii Dashkovets on 21.10.2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
